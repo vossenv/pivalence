b29bd2e8209646fce86a071573f874002c152a36
jansseneda-adobe <danimaejanssen@gmail.com>
2019-07-14 02:28:02 -0500

    xxx

---
+++ pieveilance/XXXGet.py
@@ -0,0 +1,11 @@
+
+
+
+
+from os import listdir
+from os.path import isfile, join
+
+
+
+
+print()
--- pieveilance/pieveilance.py
+++ pieveilance/pieveilance.py
@@ -9,6 +9,8 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+from pathlib import Path
+
 
 class PiWndow(QMainWindow):
 
@@ -21,9 +23,9 @@ class PiWndow(QMainWindow):
             # we are running in a normal Python environment
             bundle_dir = os.path.dirname(os.path.abspath(__file__))
 
-        self.appTitle = "Pi Valence"
-        self.stylesheetPath = os.path.join(bundle_dir,"resources", "styles.qss")
-        self.appIcon = os.path.join(bundle_dir,"resources", "bodomlogo-small.jpg")
+        self.appTitle = "Pi Veilance"
+        self.stylesheetPath = os.path.join(bundle_dir, "resources", "styles.qss")
+        self.appIcon = os.path.join(bundle_dir, "resources", "bodomlogo-small.jpg")
         self.initUI()
 
     def initUI(self):
@@ -33,7 +35,7 @@ class PiWndow(QMainWindow):
         self.show()
 
     def setGridContent(self):
-        positions = [(i, j) for i in range(10) for j in range(10)]
+        positions = [(i, j) for i in range(3) for j in range(3)]
         for p in positions:
             label = QLabel()
             s = Thread(self)
@@ -48,14 +50,12 @@ class PiWndow(QMainWindow):
         self.widget = QWidget()
         self.grid = QGridLayout()
         self.grid.setSpacing(0)
-        self.grid.setContentsMargins(0,0,0,0)
+        self.grid.setContentsMargins(0, 0, 0, 0)
         self.widget.setLayout(self.grid)
         self.setWindowTitle(self.appTitle)
         self.setWindowIcon(QIcon(self.appIcon))
         self.setCentralWidget(self.widget)
         self.statusBar().showMessage('Placeholder')
-
-
 
     def contextMenuEvent(self, event):
         cmenu = QMenu(self)
@@ -75,24 +75,24 @@ class Thread(QThread):
     changePixmap = pyqtSignal(QPixmap)
     changeLabel = pyqtSignal(str)
 
+    path = "E:\\Pics"
+    plist = [f for f in Path(path).glob('**/*.jpg')]
+
+    def update(self):
+        qp = QPixmap(str(random.choice(self.plist))).scaled(500, 500)
+        return qp
+
+    # def update(self):
+    #     img = requests.get("https://picsum.photos/500").content
+    #     qp = QPixmap()
+    #     qp.loadFromData(img)
+    #     return qp
 
     def run(self):
-        sleep = random.randint(1, 50) / 150
+        self.sleep = random.randint(10, 50)*.07
         while True:
-            img = requests.get("https://picsum.photos/500").content
-            qp = QPixmap()
-            qp.loadFromData(img)
-            self.changePixmap.emit(qp)
-
-
-            # data = ""
-            # for i in range(0,5):
-            #     r = str(random.randint(5e12,5e13))
-            #     r = base64.b64encode(r.encode('ascii'))
-            #     data += r.decode()+ "\n"
-            # self.changeLabel.emit(data + "\nSleep = " + str(sleep))
-            # time.sleep(sleep)
-
+            self.changePixmap.emit(self.update())
+            time.sleep(self.sleep)
 
 
 if __name__ == '__main__':
